21dcf57ade9e63df242ea54f7af5f044209d8e5b
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-16 22:02:24 +0200

    feat(db): convert postAddProduct to use mongodb

--- app.js
+++ app.js
@@ -4,34 +4,34 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database');
+const mongoConnect = require('./util/database').mongoConnect;
 
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const adminRoutes = require('./routes/admin');
-// const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   // User.findById(1)
-//   //   .then(user => {
-//   //     req.user = user;
-//   //     next();
-//   //   })
-//   //   .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  // User.findById(1)
+  //   .then(user => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
+});
 
-//app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect((client) => {
-  console.log(client);
+mongoConnect(() => {
   app.listen(3000);
 })
