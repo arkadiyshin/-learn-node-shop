fa5d1f8ac811e7bfd833dc6277362ca7c32e8a3f
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-16 19:12:40 +0200

    refactor(db): switch from sequlize to mongodb

--- util/database.js
+++ util/database.js
@@ -1,8 +1,17 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+require('dotenv').config();
 
-const sequelize = new Sequelize('node-complete', 'root', 'nodecomplete', {
-  dialect: 'mysql',
-  host: 'localhost'
-});
+const mongoConnect = cb => {
+  MongoClient
+    .connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.igv9dig.mongodb.net/?retryWrites=true&w=majority`)
+    .then(result => {
+      console.log('connected to db!')
+      cb(result);
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
