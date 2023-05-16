1a3d2f39350ee4f55f794f99ecbc97ef24be6663
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-16 20:58:07 +0200

    feat(db): create mongodb connection and store client to _db

--- util/database.js
+++ util/database.js
@@ -2,16 +2,28 @@ const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
 require('dotenv').config();
 
-const mongoConnect = cb => {
+let _db;
+
+const mongoConnect = callback => {
   MongoClient
     .connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.igv9dig.mongodb.net/?retryWrites=true&w=majority`)
-    .then(result => {
+    .then(client => {
       console.log('connected to db!')
-      cb(result);
+      _db = client.db();
+      callback();
     })
     .catch(err => {
       console.log(err)
+      throw err;
     })
 }
 
-module.exports = mongoConnect;
+const getDb = () => {
+  if (_db) {
+    return _db;
+  } else {
+    throw 'no database found';
+  }
+}
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
