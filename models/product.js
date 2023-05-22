abfbe9eaa5d949a630bbb4134c1721be32f06082
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-22 18:40:48 +0200

    fix(db): check id before convert to ObjectId

--- models/product.js
+++ models/product.js
@@ -2,12 +2,12 @@ const mongodb = require('mongodb')
 const getDb = require('../util/database').getDb;
 
 class Product {
-  constructor(title, price, description, imageUrl, _id) {
+  constructor(title, price, description, imageUrl, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
-    this._id = new mongodb.ObjectId(_id);
+    this._id = id ? new mongodb.ObjectId(id) : null;
   }
 
   save() {
