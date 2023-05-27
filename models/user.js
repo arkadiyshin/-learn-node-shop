6770e03ce49343e79b2987216e869108d723b045
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-27 22:29:08 +0200

    feat(shop): create order

--- models/user.js
+++ models/user.js
@@ -95,6 +95,22 @@ class User {
       });
   }
 
+  addOrder() {
+    const db = getDb();
+    return db
+      .collection('orders')
+      .insertOne(this.cart)
+      .then(result => {
+        this.cart = [];
+        db
+          .collection('users')
+          .updateOne(
+            { _id: new mongodb.ObjectId(this._id) },
+            { $set: { cart: { items: [] } } }
+          )
+      })
+  }
+
   static findById(userId) {
     const db = getDb()
     return db
