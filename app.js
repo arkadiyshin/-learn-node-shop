59dd2fe5f5576d30bf57ee3fd20a01ab3c1d82d0
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-29 15:03:41 +0200

    refactor(db): convert product model to mongoose schema

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const errorController = require('./controllers/error');
 //const mongoConnect = require('./util/database').mongoConnect;
 const mongoose = require('mongoose')
 
+require('dotenv').config();
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -14,18 +16,19 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-const User = require('./models/user');
+// const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  User.findById('6473a0c2cd245f4699c62001')
-    .then(user => {
-      req.user = new User(user.name, user.email, user._id, user.cart);
-      next();
-    })
-    .catch(err => console.log(err));
+  // User.findById('6473a0c2cd245f4699c62001')
+  //   .then(user => {
+  //     req.user = new User(user.name, user.email, user._id, user.cart);
+  //     next();
+  //   })
+  //   .catch(err => console.log(err));
+  next();
 });
 
 app.use('/admin', adminRoutes);
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,14 +13,11 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(title, price, description, imageUrl);
+
+  const product = new Product({ title, price, description, imageUrl });
+
   product
-    .save({
-      title: title,
-      price: price,
-      imageUrl: imageUrl,
-      description: description
-    })
+    .save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
