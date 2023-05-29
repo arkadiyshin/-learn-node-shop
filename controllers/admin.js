42247f9ad07b24a1ed7380f79bd64f1f76740ef5
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-29 20:38:21 +0200

    refactor(db): fetching relations

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,8 +13,9 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
+  const userId = req.user;
 
-  const product = new Product({ title, price, description, imageUrl });
+  const product = new Product({ title, price, description, imageUrl, userId });
 
   product
     .save()
