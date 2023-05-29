0ac3dc1d0712d11aa4e0b8ceef390f346fc7e1c7
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-29 21:02:41 +0200

    refactor(db): convert getCart to using mongoose

--- controllers/shop.js
+++ controllers/shop.js
@@ -43,8 +43,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
       res.render('shop/cart', {
         path: '/cart',
         pageTitle: 'Your Cart',
