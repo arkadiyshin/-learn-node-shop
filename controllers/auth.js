1ab069a03ed80338ffde0fe5c9dad4dd98524ed0
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-05-31 17:43:36 +0200

    feat(auth): add postLogin withoutt checking password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,24 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login'
   });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email })
+    .then(result => {
+      console.log(result)
+      // TODO: check password
+      res.redirect('/');
+
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
