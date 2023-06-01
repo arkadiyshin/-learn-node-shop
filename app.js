b01d246805f1796763c2c3c9260c4b8edf6264b0
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-01 13:18:45 +0200

    feat(auth): apply session

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const session = require('express-session');
 
 require('dotenv').config();
 
@@ -21,6 +22,9 @@ const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+  session({ secret: 'my secret', resave: false, saveUninitialized: false })
+);
 
 app.use((req, res, next) => {
   User.findById('6474b05db22ab4b939c74c9e')
