d2a1a2bb3f239c5386acb46a96949689cb3682b1
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-01 21:26:47 +0200

    feat(auth): encrypt password

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,6 @@
+const bcrypt = require('bcryptjs')
 const User = require('../models/user');
+
 
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', {
@@ -56,9 +58,12 @@ exports.postSignUp = (req, res, next) => {
       if (userDoc) {
         return res.redirect('/signup')
       }
+      return bcrypt.hash(password, 12);
+    })
+    .then(hashedPassword => {
       const user = new User({
         email: email,
-        password: password,
+        password: hashedPassword,
         cart: { items: [] }
       })
       return user.save();
