9229e36b31131206be577d6d223f47179a27f1b5
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-12 18:27:30 +0200

    feat(error): add express error handling middleware

--- app.js
+++ app.js
@@ -50,7 +50,9 @@ app.use((req, res, next) => {
       req.user = user;
       next()
     })
-    .catch(err => console.log(err))
+    .catch(err => {
+      throw new Error(err)
+    })
 })
 
 app.use((req, res, next) => {
@@ -62,7 +64,13 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
 
+app.use('/500', errorController.get500);
+
 app.use(errorController.get404);
+
+// app.use((error, req, res, next) => {
+//   res.redirect('/500')
+// });
 
 mongoose
   .connect(MONGODB_URI)
