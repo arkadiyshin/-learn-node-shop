e78f9a88093dc233db746274ce7eb58a150df02e
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-22 20:21:03 +0200

    feat(file): create pdf with order data

--- controllers/shop.js
+++ controllers/shop.js
@@ -137,7 +137,19 @@ exports.getInvoice = (req, res, next) => {
       res.setHeader('Content-Disposition', 'inline; attachment; filename="' + invoiceName + '"');
       pdfDoc.pipe(res);
 
-      pdfDoc.text('Hello world!');
+      // pdfDoc.text('Hello world!');
+      pdfDoc.fontSize(26).text('Invoice', {
+        underline: true
+      });
+      pdfDoc.text('-------------------------')
+      let totalPrice = 0;
+      order.products.forEach(prod => {
+        totalPrice += prod.quantity * prod.product.price;
+        pdfDoc.fontSize(14).text(`${prod.product.title} = ${prod.quantity} x $${prod.product.price}`)
+      })
+      pdfDoc.text('-------------------------')
+      pdfDoc.fontSize(20).text(`Total price: $${totalPrice}`);
+
       pdfDoc.end();
       // fs.readFile(invoicePath, (err, data) => {
       //   if (err) {
