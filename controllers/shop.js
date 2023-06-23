8b1a3fef6aae59ccfc22d881ba97e30cdd59b2ee
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-23 20:40:15 +0200

    feat(pagination): re-use pagination

--- controllers/shop.js
+++ controllers/shop.js
@@ -5,21 +5,37 @@ const PDFDocument = require('pdfkit')
 const Product = require('../models/product');
 const Order = require('../models/order');
 
-const ITEMS_PER_PAGE = 2;
+const ITEMS_PER_PAGE = 1;
 
 exports.getProducts = (req, res, next) => {
-  Product.find()
+  const page = +req.query.page || 1;
+  let totalItems = 0;
+
+  Product
+    .find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE)
+    })
+
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
         pageTitle: 'All Products',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
-      const error = new Error(err);
-      error.httpStatusCode = 500;
-      return next(error);
+      return next(err);
     });
 };
 
@@ -33,7 +49,9 @@ exports.getProduct = (req, res, next) => {
         path: '/products',
       });
     })
-    .catch(err => console.log(err));
+    .catch(err => {
+      return next(err);
+    });
 };
 
 exports.getIndex = (req, res, next) => {
