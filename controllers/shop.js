14bda7af657fc936a20854b5157a1aa3b46eb236
arkadiyshin <arkadiy-shyn_student2022@wilder.school>
2023-06-25 12:36:03 +0200

    feat(payment): add checkoout  page

--- controllers/shop.js
+++ controllers/shop.js
@@ -128,6 +128,29 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .then(user => {
+      const products = user.cart.items;
+      let total = 0;
+      products.forEach(p => {
+        total += p.quantity * p.productId.price;
+      })
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum: total,
+      });
+    })
+    .catch(err => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+}
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
